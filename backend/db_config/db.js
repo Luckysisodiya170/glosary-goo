55b2d73bac32b0bcfb60ef4e42eb92a50936bb61
Rajveer329 <rajveersinghsisodiya329@gmail.com>
2025-03-27 23:23:30 +0530

    Connected new repository

--- backend/db_config/db.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-
-const uri_string = process.env.MONGODB_URI;
-
-const connection = mongoose.connect(uri_string);
-
-module.exports = connection;
